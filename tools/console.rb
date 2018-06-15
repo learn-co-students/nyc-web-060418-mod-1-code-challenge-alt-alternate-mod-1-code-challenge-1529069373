a0fc359a47931131cfdd3e556ab5cecbeb5df8df
dzeit <dzeit216@gmail.com>
2018-06-15 10:39:50 -0400

    done

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,21 @@ def reload
   load 'config/environment.rb'
 end
 
+#Authors
+david = Author.new("David")
+tashawn = Author.new("Tashawn")
+graham = Author.new("Graham")
+
+#Magazines
+time = Magazine.new("Time", "News")
+si = Magazine.new("SI", "Sports")
+forbes = Magazine.new("Forbes", "Business")
+
+#Articles
+my_code_challenge = Article.new("My Code Challenge", david, time)
+my_trippin_reverse_jam = Article.new("My Trippin Reverse Jam", tashawn, si)
+im_a_tech_mogul = Article.new("I'm a Tech Mogul", graham, forbes) 
+
+
+
 Pry.start
